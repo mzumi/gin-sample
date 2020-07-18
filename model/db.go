be5d780719060d1faf02f3a8745c6b18167f4603
mzumi <masazumi.m@gmail.com>
2020-07-18 10:17:05 +0900

    test

--- model/db.go
+++ model/db.go
@@ -12,7 +12,7 @@ var db *gorm.DB
 
 func init() {
 	var err error
-	connect := fmt.Sprintf("host=%s port=%s user=%s dbname=%s sslmode=%s", os.Getenv("DB_HOST"), os.Getenv("DB_PORT"), os.Getenv("DB_USER"), os.Getenv("DB_NAME"), os.Getenv("DB_SSLMODE"))
+	connect := fmt.Sprintf("host=%s port=%s user=%s dbname=%s password=%s sslmode=%s", os.Getenv("DB_HOST"), os.Getenv("DB_PORT"), os.Getenv("DB_USER"), os.Getenv("DB_NAME"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_SSLMODE"))
 	db, err = gorm.Open("postgres", connect)
 	if err != nil {
 		fmt.Println(connect)
