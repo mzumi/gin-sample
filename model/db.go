aef261fe4c78ba5d9f34dfe5c1981b5439320633
mzumi <masazumi.m@gmail.com>
2020-07-18 10:11:30 +0900

    Add debug code

--- model/db.go
+++ model/db.go
@@ -15,6 +15,7 @@ func init() {
 	connect := fmt.Sprintf("host=%s port=%s user=%s dbname=%s password=%s sslmode=%s", os.Getenv("DB_HOST"), os.Getenv("DB_PORT"), os.Getenv("DB_USER"), os.Getenv("DB_NAME"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_SSLMODE"))
 	db, err = gorm.Open("postgres", connect)
 	if err != nil {
+		fmt.Println(connect)
 		fmt.Println(err.Error())
 		panic("failed to connect database")
 	}
